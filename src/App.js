0b095053fc2711fe5089cf631bf6854de95991bc
Ashish Salve <ashishsalve1777@gmail.com>
2021-09-30 00:38:59 +0530

    video 15 favicon and title changed

--- src/App.js
+++ src/App.js
@@ -26,11 +26,21 @@ function App() {
       document.body.style.backgroundColor = "#343a40";
       // document.body.style.color = "white"; // My code working
       showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+
+      // Never use below code in production it is just for understanding
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazimg";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Download TextUtils Now";
+      // }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       // document.body.style.color = "black";  // My code working
       showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
   };
 
