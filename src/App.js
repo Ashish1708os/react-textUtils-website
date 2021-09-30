10450fed82966197b669489b06e1af3a89db8eb7
Ashish Salve <ashishsalve1777@gmail.com>
2021-09-30 19:25:19 +0530

    video 17 Router and About page removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); // wheather dark mode is enabled or not
@@ -49,24 +49,24 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
 
-      <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Switch>
+      {/* <Router> */}
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Switch>
             <Route exact path="/about">
               <About />
-            </Route>
-            <Route exact path="/">
-              <TextForm
-                heading="Enter the text for analyze below"
-                mode={mode}
-                showAlert={showAlert}
-              />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+            </Route> */}
+        {/* <Route exact path="/"> */}
+        <TextForm
+          heading="Enter the text for analyze below"
+          mode={mode}
+          showAlert={showAlert}
+        />
+        {/* </Route> */}
+        {/* </Switch> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
